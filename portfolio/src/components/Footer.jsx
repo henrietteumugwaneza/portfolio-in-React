0b5c33211941efa3cc69806af159409fbfa46f4e
henrietteumugwaneza <umugwanezahenriette13@gmail.com>
2026-03-14 18:56:51 +0200

    footer

---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+  return(
+    <footer style={{textAlign:"center",padding:"20px",background:"#020617",color:"#94a3b8"}}>
+      © 2026 HenrichDev. All rights reserved.
+    </footer>
+  )
+}
+
+export default Footer
